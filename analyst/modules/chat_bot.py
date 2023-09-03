4f08a91e77f1441195e412961bd79cf7db1a595c
wilbrone <wilbroneokoth@gmail.com>
2023-09-03 04:46:53 +0300

    add more setting for reading files

--- analyst/modules/chat_bot.py
+++ analyst/modules/chat_bot.py
@@ -33,6 +33,8 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=10
 docs = text_splitter.create_documents(df)
 embeddings = OpenAIEmbedding(model="text-embedding-ada-002")
 
+full_file_path = os.path.join(settings.MEDIA_ROOT, '1YearCLEANED.csv')
+
 # my_activeloop_org_id = settings.ACTIVELOOP_USERNAME
 # my_activeloop_dataset_name = "data_sense_titanic_train"
 # dataset_path = f"hub://{my_activeloop_org_id}/{my_activeloop_dataset_name}"
@@ -75,13 +77,18 @@ def chat_bot_qeustion_predictions():
     except Exception as e:
         print(e)
 
+
+
 def send_to_general(question):
     try:
         # Create a session and reuse it for multiple requests if needed
-        with CodeInterpreterSession() as session:
+        with CodeInterpreterSession(model="gpt-3.5-turbo-16k") as session:
             # Define the user request
             user_request = question
-            files = []  # Add files if needed
+            files = [
+                # File(full_file_path)
+                File.from_path(full_file_path),
+            ]  # Add files if needed
             
             chat_history = get_chat_history()
             # Extend the chat history with the user's request
