9659db136d3d4a755024617abc28669a4d0e14e9
wilbrone <wilbroneokoth@gmail.com>
2023-09-15 18:35:09 +0300

    add code clean up

--- analysist/settings.py
+++ analysist/settings.py
@@ -176,7 +176,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
