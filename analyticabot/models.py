cad804b75a62ecd8826882a0d4d87cc3f0ec0805
wilbrone <wilbroneokoth@gmail.com>
2023-09-15 17:10:37 +0300

    add code clean up for the deployment

--- analyticabot/models.py
+++ analyticabot/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class UploadedFile(models.Model):
-    file_name = models.CharField(max_length=255)
-    file_path = models.FileField(upload_to='uploads/')
