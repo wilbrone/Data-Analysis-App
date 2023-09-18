18006207123a6cfc769192f49d022d75775f3953
wilbrone <wilbroneokoth@gmail.com>
2023-09-19 01:25:36 +0300

    add more db config

--- analysist/settings.py
+++ analysist/settings.py
@@ -105,18 +105,28 @@ else:
 # }
 
 # development
-if config('MODE')=="dev":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': '5432',
-        }
+# if config('MODE')=="dev":
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '5432',
     }
+}
+# else:
+#     DATABASES = {
+#         'default': dj_database_url.config(
+#             default=config('DATABASE_URL')
+#         )
+#     }
 
+#     db_from_env = dj_database_url.config(conn_max_age=500)
+#     DATABASES['default'].update(db_from_env)
+
+    # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 os.environ["OPENAI_API_KEY"] = config("OPENAI_API_KEY")
